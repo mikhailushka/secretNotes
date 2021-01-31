7691ddc937287e3c42982b4e572b5fabbd22c1e4
mikhailushka <mihailgeorgievskij@gmail.com>
2021-02-01 01:36:17 +0300

    fix: remove redundant code

--- client/src/components/GetNote.vue
+++ client/src/components/GetNote.vue
@@ -71,8 +71,6 @@ export default {
           password: this.password
         })
 
-        debugger;
-
         if (response.data.password === 'invalid') {
           this.flashMessage.error({title: 'Действие с записью', message: 'Вы ввели не верный пароль'})
         } else {
